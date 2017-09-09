65a11eb43c67ba079826d621ed592b684522698b
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 01:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,4 +65,4 @@ function Quadrilateral(sides) {
 
 Quadrilateral.prototype = Object.create(Polygon.prototype);
 Quadrilateral.prototype.constructor = Quadrilateral;
-Quadrilateral.prototype.numberOfSides = 4
+Quadrilateral.prototype.numberOfSides = 4;
